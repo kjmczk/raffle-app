cf1d254c6a0892abcaac3ef28b278cce5563b712
kjmczk <vollgong@gmail.com>
2021-12-21 19:24:09 +0900

    Add features: 1 click removing, User sorting

--- pages/data/[slug].tsx
+++ pages/data/[slug].tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { FormEvent } from 'react';
+import { useEffect, useState } from 'react';
 import useSWR, { useSWRConfig } from 'swr';
+import { FaTimesCircle, FaAngleDown, FaAngleUp } from 'react-icons/fa';
 
 import ModalRenameFile from '../../components/ModalRenameFile';
 import ModalDeleteFile from '../../components/ModalDeleteFile';
 import ModalRenameTitle from '../../components/ModalRenameTitle';
 
 export default function Data() {
+  const [users, setUsers] = useState<string[]>([]);
+  const [sortedUsers, setSortedUsers] = useState<string[] | null>(null);
+
   const router = useRouter();
   const { mutate } = useSWRConfig();
   const { data, error } = useSWR(
@@ -34,10 +39,7 @@ export default function Data() {
     event.currentTarget.newItem.value = '';
   }
 
-  async function handleRemoveItem(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const value = event.currentTarget.rmItem.value;
-    const rmItem = value.trim();
+  async function handleRemoveItem(rmItem: string) {
     mutate(`/api/data/${router.query.slug}`, async () => {
       await fetch(`/api/data/${router.query.slug}`, {
         method: 'PATCH',
@@ -51,7 +53,6 @@ export default function Data() {
         }),
       });
     });
-    event.currentTarget.rmItem.value = '';
   }
 
   async function handleAddUser(event: FormEvent<HTMLFormElement>) {
@@ -74,10 +75,7 @@ export default function Data() {
     event.currentTarget.newUser.value = '';
   }
 
-  async function handleRemoveUser(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const value = event.currentTarget.rmUser.value;
-    const rmUser = value.trim();
+  async function handleRemoveUser(rmUser: string) {
     mutate(`/api/data/${router.query.slug}`, async () => {
       await fetch(`/api/data/${router.query.slug}`, {
         method: 'PATCH',
@@ -91,8 +89,28 @@ export default function Data() {
         }),
       });
     });
-    event.currentTarget.rmUser.value = '';
   }
+
+  function sortUsers() {
+    setSortedUsers(users.sort());
+  }
+
+  function reverseUsers() {
+    setSortedUsers(users.sort().reverse());
+  }
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data.users);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (sortedUsers) {
+      setUsers(sortedUsers);
+      setSortedUsers(null);
+    }
+  }, [sortedUsers]);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
@@ -142,26 +160,11 @@ export default function Data() {
             id="newItem"
             name="newItem"
             placeholder="Item name"
+            required
             className="appearance-none bg-gray-200 text-gray-700 text-sm border border-gray-200 rounded py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mr-1"
           />
           <button className="rounded px-2 py-1 bg-gray-700 hover:bg-gray-600 text-sm text-gray-300">
             Add
-          </button>
-        </form>
-
-        <form onSubmit={handleRemoveItem}>
-          <label htmlFor="rmItem" className="block text-sm mb-2">
-            Enter the item to remove:
-          </label>
-          <input
-            type="text"
-            id="rmItem"
-            name="rmItem"
-            placeholder="Item name"
-            className="appearance-none bg-gray-200 text-gray-700 text-sm border border-red-600 rounded py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-red-500 mr-1"
-          />
-          <button className="rounded px-2 py-1 bg-red-600 hover:bg-red-700 text-sm text-gray-200">
-            Remove
           </button>
         </form>
       </div>
@@ -173,9 +176,12 @@ export default function Data() {
               {data.items.map((item: string, i: number) => (
                 <span
                   key={i}
-                  className="text-gray-300 font-bold inline-block border border-gray-500 rounded px-2"
+                  className="inline-flex items-center border border-gray-500 rounded px-2"
                 >
-                  {item}
+                  <span className="text-gray-300 font-bold mr-2">{item}</span>
+                  <button onClick={() => handleRemoveItem(item)}>
+                    <FaTimesCircle />
+                  </button>
                 </span>
               ))}
             </div>
@@ -186,7 +192,7 @@ export default function Data() {
       </div>
 
       <div>
-        <div className="flex space-x-4 mb-2">
+        <div className="flex justify-between items-end space-x-4 mb-2">
           <form onSubmit={handleAddUser}>
             <label htmlFor="newUser" className="block text-sm mb-2">
               Enter the user to add:
@@ -196,6 +202,7 @@ export default function Data() {
               id="newUser"
               name="newUser"
               placeholder="Username"
+              required
               className="appearance-none bg-gray-200 text-gray-700 text-sm border border-gray-200 rounded py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mr-1"
             />
             <button className="rounded px-2 py-1 bg-gray-700 hover:bg-gray-600 text-sm text-gray-300">
@@ -203,39 +210,53 @@ export default function Data() {
             </button>
           </form>
 
-          <form onSubmit={handleRemoveUser}>
-            <label htmlFor="rmUser" className="block text-sm mb-2">
-              Enter the user to remove:
-            </label>
-            <input
-              type="text"
-              id="rmUser"
-              name="rmUser"
-              placeholder="Username"
-              className="appearance-none bg-gray-200 text-gray-700 text-sm border border-red-600 rounded py-1 px-2 leading-tight focus:outline-none focus:bg-white focus:border-red-500 mr-1"
-            />
-            <button className="rounded px-2 py-1 bg-red-600 hover:bg-red-700 text-sm text-gray-200">
-              Remove
+          <div className="space-x-2">
+            <button
+              onClick={sortUsers}
+              className="inline-flex items-center space-x-1 hover:text-gray-300"
+            >
+              <span>Sort</span>
+              <span>
+                <FaAngleDown />
+              </span>
             </button>
-          </form>
+            <span className="text-gray-500">|</span>
+            <button
+              onClick={reverseUsers}
+              className="inline-flex items-center space-x-1 hover:text-gray-300"
+            >
+              <span>Reverse</span>
+              <span>
+                <FaAngleUp />
+              </span>
+            </button>
+          </div>
         </div>
 
-        {data.users.length > 0 ? (
+        {users.length > 0 ? (
           <table className="border-collapse table-fixed w-full">
             <thead>
               <tr>
-                <th className="w-1/5 px-3 py-1">Num</th>
-                <th className="w-4/5 px-3 py-1">Users</th>
+                <th className="w-2/12 px-3 py-1">Num</th>
+                <th className="w-9/12 px-3 py-1">Users</th>
+                <th className="w-1/12"></th>
               </tr>
             </thead>
             <tbody>
-              {data.users.map((user: string, index: number) => (
+              {users.map((user: string, index: number) => (
                 <tr key={index}>
                   <td className="border border-gray-500 text-center px-3 py-1">
                     {index + 1}
                   </td>
                   <td className="border border-gray-500 text-center text-gray-300 px-3 py-1">
                     {user}
+                  </td>
+                  <td className="">
+                    <span className="flex justify-center">
+                      <button onClick={() => handleRemoveUser(user)}>
+                        <FaTimesCircle />
+                      </button>
+                    </span>
                   </td>
                 </tr>
               ))}
