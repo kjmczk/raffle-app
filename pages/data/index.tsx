fa085219fde0c6592093c4134a0de72a4916aa0d
kjmczk <vollgong@gmail.com>
2021-12-22 01:52:56 +0900

    Improve styles

--- pages/data/index.tsx
+++ pages/data/index.tsx
@@ -10,8 +10,8 @@ export default function AllData() {
   if (!data) return <div>loading...</div>;
 
   return (
-    <div className="max-w-lg mx-auto px-4">
-      <div className="flex justify-between items-center pt-8 pb-4 border-b mb-4">
+    <div className="max-w-2xl mx-auto px-4">
+      <div className="flex justify-between items-center pt-8 pb-4 border-b border-gray-500 mb-4">
         <h1 className="text-2xl">Data List</h1>
         <Link href="/">
           <a className="text-blue-400 hover:text-blue-500">Raffle</a>
@@ -25,7 +25,7 @@ export default function AllData() {
           {data.map((data: string, index: number) => (
             <li key={index}>
               <Link href={`/data/${data}`}>
-                <a className="underline">{data}</a>
+                <a className="underline text-gray-300">{data}</a>
               </Link>
             </li>
           ))}
